402168fb19ca3f18392718a1cdb8bdd611f3ad4b
Anastasia Legert <legertlab@gmail.com>
2020-03-18 15:43:50 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Anastasia Legert on 18/3/20.
+//  Copyright © 2020 Anastasia Legert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
